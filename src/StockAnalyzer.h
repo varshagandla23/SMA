d793bdd8512dfecc2f05fea59ddb1bfbc5d465e1
raman ch <ramanch1011@gmail.com>
2026-03-29 00:34:42 +0530

    Update StockAnalyzer.h

--- src/StockAnalyzer.h
+++ src/StockAnalyzer.h
@@ -1,1 +1,8 @@
-// StockAnalyzer header 
+#pragma once
+#include <vector>
+
+class StockAnalyzer {
+public:
+    double calculateMovingAverage(const std::vector<double>& prices, int window);
+    double maxProfit(const std::vector<double>& prices);
+};// StockAnalyzer header 
